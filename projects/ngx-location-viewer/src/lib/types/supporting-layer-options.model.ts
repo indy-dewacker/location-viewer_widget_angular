3cfe4cfeb2f85e5f5593045509cb64ae55f3d6a6
Glenn Vissers <glenn.vissers@digipolis.be>
2021-08-12 17:46:38 +0200

    Feat: add default visible property to supportinglayeroptions

--- projects/ngx-location-viewer/src/lib/types/supporting-layer-options.model.ts
+++ projects/ngx-location-viewer/src/lib/types/supporting-layer-options.model.ts
@@ -1,4 +1,6 @@
 export interface SupportingLayerOptions {
     url: string;
     layerIds: number[];
+    //overrides the default visibility property of layer on mapserver: for all layers (true: visible, false:hidden)
+    visible?: boolean;
 }
