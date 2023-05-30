1ca417109c8631f2ee64859fe355c3a0f7329f7c
Nicolas T <nicolas@yapla.be>
2023-05-30 09:44:33 +0200

    Update ngx-leaflet / fix requires

--- projects/ngx-location-viewer/src/lib/services/location-viewer-map.service.ts
+++ projects/ngx-location-viewer/src/lib/services/location-viewer-map.service.ts
@@ -1,19 +1,20 @@
 import { MapService } from '@acpaas-ui/ngx-leaflet';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import 'leaflet.markercluster';
+import '@geoman-io/leaflet-geoman-free';
+import * as esriLeafletCluster from 'esri-leaflet-cluster';
 
 @Injectable()
 export class LocationViewerMapService extends MapService {
-  // tslint:disable-next-line: ban-types
-  constructor(@Inject(PLATFORM_ID) private PlatformId: Object) {
-    super(PlatformId);
-    this.requireDependencies();
-  }
-
-  requireDependencies() {
-    if (this.isAvailable()) {
-      require('@geoman-io/leaflet-geoman-free');
-      require('leaflet.markercluster');
-      this.esri.Cluster = require('esri-leaflet-cluster');
+    // tslint:disable-next-line: ban-types
+    constructor(@Inject(PLATFORM_ID) private PlatformId: Object) {
+        super(PlatformId);
+        this.requireDependencies();
     }
-  }
+
+    requireDependencies() {
+        if (this.isAvailable()) {
+            this.esri.Cluster = esriLeafletCluster;
+        }
+    }
 }
